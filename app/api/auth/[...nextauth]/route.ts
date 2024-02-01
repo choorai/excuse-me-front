65b12e70634e7c0b7ad721271f7311041d361411
Seokju Hong <iamseokjuhong@gmail.com>
2024-02-01 23:30:01 +0900

    로그인 구현해보기

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,6 @@
 import NextAuth from "next-auth";
-import {authOptions} from "@/app/lib/auth/auth";
+import {authOptions} from "@/server/auth";
 
 const handler = NextAuth(authOptions);
 
 export {handler as GET, handler as POST};
-
-// export default NextAuth({
-//     providers: [
-//         CredentialsProvider({
-//             name: "Credentials",
-//             credentials: {
-//                 username: {label: "Username", type: "text", placeholder: "jsmith"},
-//                 password: {label: "Password", type: "password"}
-//             },
-//             async authorize(credentials, req) {
-//                 const user = {id: "1", name: "J Smith", email: "jsmith@example.com"}
-//
-//                 if (user) {
-//                     return user;
-//                 } else {
-//                     return null;
-//                 }
-//             }
-//         })
-//     ]
-// })
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import Image from "next/image";
 import Link from "next/link";
-import {getServerAuthSession} from "@/app/lib/auth/auth";
-import UserInfo from "@/app/components/UserInfo";
+import {getServerAuthSession} from "@/server/auth";
+import UserInfo from "@/components/UserInfo";
 
 export default async function Home() {
-    const authSession = await getServerAuthSession();
+  const authSession = await getServerAuthSession();
 
-    return (
-        <main className="flex items-center justify-center h-screen">
-            {authSession?.user && <UserInfo user={authSession?.user}/>}
-            {!authSession?.user && (
-                <Link className="font-medium mt-2 text-blue-600 hover:underline" href="/login">
-                    Login
-                </Link>
-            )}
-        </main>
-    );
+  return (
+    <main className="flex items-center justify-center h-screen">
+      {authSession?.user && <UserInfo user={authSession?.user}/>}
+      {!authSession?.user && (
+        <Link className="font-medium mt-2 text-blue-600 hover:underline" href="/login">
+          Login
+        </Link>
+      )}
+    </main>
+  );
 }
