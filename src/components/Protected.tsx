ead649cf2e158fb776dff03fe673f41cc915223c
Seokju Hong <iamseokjuhong@gmail.com>
2024-02-05 01:03:02 +0900

    로그인 구현해보기

---
+++ src/components/Protected.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navigate } from "react-router";
+import {useAuth} from "../hooks/useAuth";
+
+interface ProtectedPageProps {
+  element: React.ReactNode;
+  option: boolean;
+}
+
+// @ts-ignore
+const Protected: React.FC<ProtectedPageProps> = ({
+                                                   element,
+                                                   option,
+                                                 }: ProtectedPageProps) => {
+  const { isLoggedIn } = useAuth();
+
+  if (isLoggedIn && !option) {
+    return <Navigate to="/home" />;
+  }
+  if (!isLoggedIn && option) {
+    return <Navigate to="/login" />;
+  }
+
+  return element;
+};
+
+export default Protected;
