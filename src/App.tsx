e5dba3d207558a740b87edb63e98d193e06bc514
Seokju Hong <iamseokjuhong@gmail.com>
2024-02-06 00:12:06 +0900

    로그인 구현해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Home} from "./components/Home";
 import {Login} from "./components/Login";
 import {SignUp} from "./components/SignUp";
-import {Route, Routes} from "react-router";
 import Protected from "./components/Protected";
-import {Link} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Nav} from "./pages/Nav";
 
 const LoginRoutes = [
   {path: "/home", component: <Home/>},
@@ -17,27 +17,12 @@ const LogoutRoutes = [
 ];
 
 function App() {
+  const [userInfo, setUserInfo] = useState("seokju");
   return (
-    <>
-      <div>무야호</div>
-      <Link to="..">돌아가!!</Link>
-      <Routes>
-        {LogoutRoutes.map(({path, component}) => (
-          <Route
-            key={path}
-            path={path}
-            element={<Protected element={component} option={false}/>}
-          />
-        ))}
-        {LoginRoutes.map(({path, component}) => (
-          <Route
-            key={path}
-            path={path}
-            element={<Protected element={component} option={true}/>}
-          />
-        ))}
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
   );
 }
 
